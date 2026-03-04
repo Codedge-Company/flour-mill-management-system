68e5739666c939e9324e1c05d26bf799e30a8f74
Thakshila-kik <thakshilam@kik.lk>
2026-03-04 17:22:50 +0530

    add prod env

--- frontend/src/app/core/services/payment-api.service.ts
+++ frontend/src/app/core/services/payment-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.prod';
 import { Payment, SaleCreditSummary, AddPaymentRequest } from '../models/payment.model';
 import { ApiResponse } from '../models/api-response';
 
