d25ab6cc3fcacce85faa21da57d941fae0088a24
Thakshila-kik <thakshilam@kik.lk>
2026-03-28 22:18:01 +0530

    change apiUrl to nginx IP

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://187.77.147.12:5000/api',
+  apiUrl: 'http://187.77.147.12/api',
   vapidPublicKey: 'BPqb7AuuJdvJ1_LGh7F4LReuHbJ0KmeosUnN9MrKcI2rEpq_1fDg-o5HF5GClGBOdj8ahCDiprOevcQvLHjwTnM'
 };
